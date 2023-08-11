df51ae20359586045369d370075796143e53b45c
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2023-08-12 00:57:49 +0530

    Added submit button functionality

--- app/src/main/java/com/example/birthdaywishes/MainActivity.kt
+++ app/src/main/java/com/example/birthdaywishes/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.birthdaywishes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun createBirthdayCard(view: View) {
+
+        val name = findViewById<EditText>(R.id.nameInput)
+        Toast.makeText(this, "Name is ${name.editableText}", Toast.LENGTH_LONG).show()
     }
 }
